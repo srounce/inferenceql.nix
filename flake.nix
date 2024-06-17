InferenceQL/nix

feat(bayes3d): init bayes3d @ ba4234a4

flake.nix
@@ -7,6 +7,9 @@
     iqlquery.url = "github:inferenceql/inferenceql.query";
   };
 
+  nixConfig.extra-substituters = [ "https://numtide.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
@@ -34,7 +37,16 @@
         };
 
         toolkit = self.lib.basicTools pkgs;
+
+        bayes3d = pkgs.callPackage ./pkgs/bayes3d { };
       in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
@@ -44,7 +56,11 @@
         };
 
         packages = {
-          inherit ociImgBase ociImgIqlQuery;
+          inherit
+            ociImgBase
+            ociImgIqlQuery
+            bayes3d
+          ;
         };
       };
 

srounce/inferenceql.nix

flake.nix
@@ -6,6 +6,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
+  nixConfig.extra-substituters = [ "https://numtide.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
@@ -33,6 +36,7 @@
 
         scopes = (self.lib.mkScopes {
           inherit pkgs;
+          self = self';
           basicTools = self.lib.basicTools;
         });
         loom = scopes.callPy3Package ./pkgs/loom { };
@@ -44,8 +48,19 @@
 
             ociImgBase
           ;
+
+          bayes3d = scopes.callPy3Package ./pkgs/bayes3d { };
+          open3d = scopes.callPy3Package ./pkgs/open3d { };
         };
       in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
+
         inherit packages;
       };
 